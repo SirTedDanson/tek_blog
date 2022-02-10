34428065edbc34230315aec3085b6e153f4496d1
sirteddanson <knowledgeMED@gmail.com>
2022-02-10 13:25:28 -0500

    home/login views created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,30 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-
-router.get('/', (req, res) => {
-  res.json(`User ${req.method} request received!`)
+// homepage
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
+// generate login page
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-router.post('/', (req, res) => {
-  res.json(`User ${req.method} request received!`)
+  res.render("login");
 });
 
-
-router.put('/', (req, res) => {
-  res.json(`User ${req.method} request received!`)
+router.post("/", (req, res) => {
+  res.json(`User ${req.method} request received!`);
 });
 
+router.put("/", (req, res) => {
+  res.json(`User ${req.method} request received!`);
+});
 
-router.delete('/', (req, res) => {
-  res.json(`User ${req.method} request received!`)
+router.delete("/", (req, res) => {
+  res.json(`User ${req.method} request received!`);
 });
 
 module.exports = router;
