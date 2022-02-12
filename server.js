5fe3b91160e46c2c6f3c12db66eaca4f4190f4fd
sirteddanson <knowledgeMED@gmail.com>
2022-02-12 11:07:59 -0500

    removed .env from .gitignore

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-const { destroy } = require("./models/User");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
